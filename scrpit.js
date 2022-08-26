041a083071415728ec36e6f6756b76c141798226
Emanuel Nogueira <manunogueira8814@gmail.com>
2022-08-26 14:46:00 -0300

    Second commit

---
+++ scrpit.js
@@ -0,0 +1,13 @@
+const form = document.getElementById("form");
+const username = document.getElementById("username");
+const email = document.getElementById("email");
+const password = document.getElementById("password");
+const passwordConfirmation = document.getElementById("password-confirmation");
+
+// Evento para meu botão
+form.addEventListener("submit", (e) => {
+  // Para não recarregar a página
+  e.preventDefault();
+});
+
+function checkInput() {}
